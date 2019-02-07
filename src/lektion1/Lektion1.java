d81d0612cc83bb784f054cec0a0191ce163f65fb
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2019-02-07 15:00:15 +0100

    Ny repository

--- src/lektion1/Lektion1.java
+++ src/lektion1/Lektion1.java
@@ -22,7 +22,7 @@ public class Lektion1 extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Ancient Cipher");
         stage.setScene(scene);
         stage.show();
     }
